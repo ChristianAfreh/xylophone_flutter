ec6ffb20ae6d2a554ec404aa9547be5e23169e57
Christian Afreh <christian.afreh@primustechgh.net>
2023-12-10 21:35:56 +0000

    adding initial completed project files

---
+++ lib/main.dart
@@ -0,0 +1,47 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  Expanded buildKey({required Color color, required int noteNumber}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          final player = AudioPlayer();
+          player.play(AssetSource('note${noteNumber}.wav'));
+        },
+        style: TextButton.styleFrom(backgroundColor: color),
+        child: Text(''),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Colors.red, noteNumber: 1),
+              buildKey(color: Colors.orange, noteNumber: 2),
+              buildKey(color: Colors.yellow, noteNumber: 3),
+              buildKey(color: Colors.green, noteNumber: 4),
+              buildKey(color: Colors.blue, noteNumber: 5),
+              buildKey(color: Colors.indigo, noteNumber: 6),
+              buildKey(color: Colors.deepPurple, noteNumber: 7),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
